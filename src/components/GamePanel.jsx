77c4858cea9805d8a5d8940b5a63e5fc07a126e6
Brian Kwok <brkwok@gmail.com>
2023-05-09 03:30:09 -0400

    Move states into panel

--- src/components/GamePanel.jsx
+++ src/components/GamePanel.jsx
@@ -4,15 +4,20 @@ import Stats from "./Stats";
 // import Hold from "./Hold";
 import Previews from "./Previews";
 import Controller from "./Controller";
+import { useBoard } from "../hooks/useBoard";
+import { useStats } from "../hooks/useStats";
+import { usePlayer } from "../hooks/usePlayer";
 
-const GamePanel = ({
-	gameOver,
-	setGameOver,
-	stats,
-	player,
-	setPlayer,
-	board
-}) => {
+const GamePanel = ({ row, col, setGameOver, resetGameOver }) => {
+	const [player, setPlayer, resetPlayer] = usePlayer();
+	const [stats, addLinesCompleted] = useStats();
+	const [board] = useBoard({
+		row,
+		col,
+		player,
+		resetPlayer,
+		addLinesCompleted,
+	});
 
 	return (
 		<div className="relative flex justify-center items-center h-fit w-fit">
@@ -24,8 +29,8 @@ const GamePanel = ({
 				board={board}
 				stats={stats}
 				player={player}
+				resetGameOver={resetGameOver}
 				setGameOver={setGameOver}
-				gameOver={gameOver}
 				setPlayer={setPlayer}
 			/>
 		</div>
--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,47 +1,28 @@
 import React from "react";
 import Menu from "./Menu";
 import { useGameOver } from "../hooks/useGameOver";
-import { useStats } from "../hooks/useStats";
-import { usePlayer } from "../hooks/usePlayer";
-import { useBoard } from "../hooks/useBoard";
 import GamePanel from "./GamePanel";
 
 const Tetris = ({ row, col }) => {
 	const [gameOver, setGameOver, resetGameOver] = useGameOver();
-	const [player, setPlayer, resetPlayer] = usePlayer();
-	const [stats, addLinesCompleted, resetStats] = useStats();
-	const [board, resetBoard] = useBoard({
-		row,
-		col,
-		player,
-		resetPlayer,
-		addLinesCompleted,
-	});
 
 	const handleStart = () => {
 		resetGameOver();
-		resetPlayer(true);
-		resetBoard();
-		resetStats();
 	};
 
 	return (
 		<div className="relative flex justify-center align-middle">
-			{gameOver ? <Menu onClick={handleStart} /> : null}
-
-			<GamePanel
-				row={row}
-				col={col}
-				resetGameOver={resetGameOver}
-				setGameOver={setGameOver}
-				gameOver={gameOver}
-				stats={stats}
-				player={player}
-				setPlayer={setPlayer}
-				resetPlayer={resetPlayer}
-				board={board}
-				addLinesCompleted={addLinesCompleted}
-			/>
+			{gameOver ? (
+				<Menu onClick={handleStart} />
+			) : (
+				<GamePanel
+					row={row}
+					col={col}
+					resetGameOver={resetGameOver}
+					setGameOver={setGameOver}
+					gameOver={gameOver}
+				/>
+			)}
 		</div>
 	);
 };
